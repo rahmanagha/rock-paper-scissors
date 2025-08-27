6dd810c57085bd7760018f72efd3754554693651
rahmanagha <aghazad.rahman@gmail.com>
2025-08-27 16:12:35 +0400

    Create function to get human choice

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ function getComputerChoice() {
     const index = Math.floor(Math.random()*3)
     return index === 0 ? "rock": index === 1 ? "paper" : "scissors"
 }
+// Write function to get human choice
+function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissors?")
+    return choice.toLowerCase()
+}
