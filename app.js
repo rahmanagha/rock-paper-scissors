c7cf475839d093110d272099cd79065a7f67b5b0
rahmanagha <aghazad.rahman@gmail.com>
2025-08-27 16:01:29 +0400

    Create function to get random choice for computer

---
+++ app.js
@@ -0,0 +1,5 @@
+// Write a function to get random choice for computer
+function getComputerChoice() {
+    const index = Math.floor(Math.random()*3)
+    return index === 0 ? "rock": index === 1 ? "paper" : "scissors"
+}
