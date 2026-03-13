a670e361bad9a22b1e1a4eeaa2762561cb113861
rahmanagha <aghazad.rahman@gmail.com>
2026-03-13 11:53:29 +0400

    Refactor computerChoice function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Write a function to get random choice for computer
 function getComputerChoice() {
-    const index = Math.floor(Math.random()*3)
-    return index === 0 ? "rock": index === 1 ? "paper" : "scissors"
+    return ["rock","paper","scissors"][Math.floor(Math.random()*3)]
 }
 // Write function to get human choice
 function getHumanChoice() {
